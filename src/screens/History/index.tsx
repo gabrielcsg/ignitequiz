9ca9a6632d0fe39199b65e1a68a71e108bed63cd
Gabriel Castro <gabrielcssg@gmail.com>
2024-06-02 23:10:25 -0300

    feat: layout animation

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import Animated, {
+  Layout,
+  SlideInRight,
+  SlideOutRight,
+} from 'react-native-reanimated';
 import { HouseLine } from 'phosphor-react-native';
 
 import { Header } from '../../components/Header';
@@ -29,17 +34,13 @@ export function History() {
   }
 
   function handleRemove(id: string) {
-    Alert.alert(
-      'Remover',
-      'Deseja remover esse registro?',
-      [
-        {
-          text: 'Sim', onPress: () => remove(id)
-        },
-        { text: 'Não', style: 'cancel' }
-      ]
-    );
-
+    Alert.alert('Remover', 'Deseja remover esse registro?', [
+      {
+        text: 'Sim',
+        onPress: () => remove(id),
+      },
+      { text: 'Não', style: 'cancel' },
+    ]);
   }
 
   useEffect(() => {
@@ -47,7 +48,7 @@ export function History() {
   }, []);
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
@@ -63,16 +64,18 @@ export function History() {
         contentContainerStyle={styles.history}
         showsVerticalScrollIndicator={false}
       >
-        {
-          history.map((item) => (
-            <TouchableOpacity
-              key={item.id}
-              onPress={() => handleRemove(item.id)}
-            >
+        {history.map((item) => (
+          <Animated.View
+            key={item.id}
+            entering={SlideInRight}
+            exiting={SlideOutRight}
+            layout={Layout.springify()}
+          >
+            <TouchableOpacity onPress={() => handleRemove(item.id)}>
               <HistoryCard data={item} />
             </TouchableOpacity>
-          ))
-        }
+          </Animated.View>
+        ))}
       </ScrollView>
     </View>
   );
